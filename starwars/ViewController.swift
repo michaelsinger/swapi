db012cd77f248785bd9b33d86660505673d23fb1
synesthesio <msinger681@gmail.com>
2019-07-06 12:45:07 -0400

    Initial Commit

---
+++ starwars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starwars
+//
+//  Created by synesthesia on 7/6/19.
+//  Copyright © 2019 anonymous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
